68ee8749cdef5ecc90ae93ac40602ac435cdd580
SAVIYA-MD <126868140+saviya55@users.noreply.github.com>
2024-01-15 18:23:42 +0530

    Remove Old Apk Dl⚙

--- commands/downloader.js
+++ commands/downloader.js
@@ -150,74 +150,7 @@ cmd({
         }
     )
     //---------------------------------------------------------------------------
-cmd({
-            pattern: "apk",
-            desc: "Downloads apks  .",
-            category: "downloader",
-	    react: "📥",
-	    react: "📤",
-            filename: __filename,
-            use: '<add sticker url.>',
-        },
 
-        async(Void, citel, text) => {
-        if(!text )return citel.reply("*Give me App Name*");
-
-	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
-	let randomName = getRandom(".apk");
-	const filePath = `./${randomName}`;     // fs.createWriteStream(`./${randomName}`)
-        const {  search , download } = require('aptoide-scraper')
-	let searc = await search(text);          //console.log(searc);
-	let data={};
-	if(searc.length){ data = await download(searc[0].id); }
-	else return citel.send("*APP not Found, Try Other Name*");
-	
-	
-	const apkSize = parseInt(data.size);
-	if(apkSize > 150) return citel.send(`❌ File size bigger than 200mb.`);
-       const url = data.dllink;
-	 let 
-         inf ="*📦SAVIYA-MD-APK-DL📦* "
-	
-         inf +="\n*🕹App Name :* " +data.name;
-         inf +="\n*🌐App id :* " +data.package;
-         inf +="\n*⬆️Last Up :* " +data.lastup;
-         inf +="\n*📥App Size :* " +data.size;
-        // inf +="\n*🔖App Link :* " +data.dllink;
-	inf +="\n\n "
-         
-
-axios.get(url, { responseType: 'stream' })
-  .then(response => {
-    const writer = fs.createWriteStream(filePath);
-    response.data.pipe(writer);
-
-    return new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
-  }).then(() => {
-	
-	let buttonMessage = {
-                        document: fs.readFileSync(filePath),
-                        mimetype: 'application/vnd.android.package-archive',
-                        fileName: data.name+`.apk`,
-                        caption : inf
-                        
-                    }
-                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-
-    console.log('File downloaded successfully');
-
-  
-    fs.unlink(filePath, (err) => {
-      if (err) { console.error('Error deleting file:', err); } else { console.log('File deleted successfully'); } });
-  }) .catch(error => {
-	fs.unlink(filePath)
-    return citel.reply('*Apk not Found, Sorry*')//:', error.message);
-  });
-}
-)
 //-------------------------------------------------------------------------------
 cmd({
             pattern: "play",
