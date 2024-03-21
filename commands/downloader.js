e40463d9fdc854d92d8ec152d3c3f5f5c20253b3
wdsaviya <128067789+wdsaviya@users.noreply.github.com>
2024-03-22 00:17:52 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -285,7 +285,41 @@ cmd({
     )
     //---------------------------------------------------------------------------
 
-
+function _0x5540()
+{
+  const _0x65ca4d = [ 'getyt', 'https://tikdown.org/id', 'greet', 'ttsCitelVoid.m4a', 'url', '<Dowanload Tiktok Sounds>', 'videoDetails', '❎ File size bigger than 200mb.', '\x09 *---Yt Song Searched Data---*   \x0a\x0aTitle : ', '9VWCHsw', 'trim', 'mediafire', 'startsWith', 'src', 'Example : ', 'caption', 'Downloads audio by yt link.', '```🥺 File Not Found```', '*_🧚 Telegram Sticker At Index ', 'Downloads audio from youtube.', 'Downloads video from yt.', '\x0aViews : ', '\x0a*Mɪᴍᴇ* : ', 'load', 'pipe', 'Downloads audio by yt link as document.', 'application/zip', '1014UUWswG', '1712572xzuZLP', 'ringtone', 'Total stickers: ', '\x0a*Url : ', 'https://github.com/DGXeon/Tiktokmusic-API/raw/master/tiktokmusic/sound', 'text', 'Url :', 'cheerio', 'content-disposition', '385902tUvNeJ', 'Yt Song Searched Data', 'get', 'chat', 'text to speech.', 'toString', 'readFileSync', 'html', '🧝‍♂️', 'ID Not Found', 'downloadUrl', '<give text>', '<url of mediafire>', '1389897APKDJS', '❎ I can\'t download that long video!', 'getInfo', 'ago', 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36', 'Give link ', '<yt video url>', '<Hii,this is Suhail>', '7797065oaKLCt', 'http', 'send', 'Sticker', 'quoted', '🧚‍♀️ Uhh Plese, Give me a Name. Ex .pint apple', 'Uhh Please, Give me  Google Drive Url', 'ytmp3 url', '\x0a*Sɪᴢᴇ* :', 'sendMessage', 'https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=', '* \x0a     *──────────────────*   \x0a', 'match', '\x0a*File Type :* ', 'title', 'Downloads ringtone.', 'music', '❎ File size bigger than 100mb.', '❎ Give youtube link!', 'timestamp', ' Downloaded_*', '1046632lBzoJK', '*YouTube Search*\x0a Result From ', '❎ File size bigger than 150mb.', 'audio/mpeg', '<faded-Alan Walker>', 'shift', 'then', '🧚 Downloads telegram stickers.', '\x0aUrl : ', '7701126tojKYX', '\x0a   ─────────────────── \x0a', 'What picture are you looking for?', '  *Here\'s Your Video*', '../lib/mediafire.js', '\x0a*Estimated complete in:* ', 'ytd', '🥺 Error While Downloading Your Song', '<add sticker url.>', '108ACetFX', 'play Pita Kaware*', 'video', 'fb-info-video', 'statSync', 'div.download-links > div:nth-child(2) > a', '❎ URL is empty! \x0aSend ', 'Photo', 'yts WhatsApp Bot by •ꜱɪᴛʜᴜᴡᴀ-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ•', 'yta', ' |  10 ,  20 ; photo', 'Sends info about the query(of youtube video/audio).', 'https://translate.google.com', '*Title : ', 'post', 'Downloads video from youtube.', 'Downloads media from Mediafire.', './ytsong.mp4', 'name', 'replace', '<faded-Alan walker.>', 'fs-extra', 'video/mp4', 'Error While Downloading Video : ', 'Downloads apks  .', '*Please give me Text*\x0a *_Example : .tts Hi,I am ProfileCorruptedError._*', 'Uhh Please, Give me Valid Google Drive Url', 'song', 'ownername', 'secktor-pack', '1523950KcTWbR', 'split', 'attr', 'gitclone', '.mp4', '\x0a*File Size :* ', 'lengthSeconds', 'entries', '_Enter a tg sticker url_\x0aEg: .tgs https://t.me/addstickers/Oldboyfinal\x0aKeep in mind that there is a chance of ban if used frequently', '\x0aUploaded : ', 'https://api.github.com/repos/', '* to *', 'div.download-links > div:nth-child(1) > a', 'status', 'includes', '  •ꜱɪᴛʜᴜᴡᴀ-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ• ', '```❎ Give Me A Number From 1 to 160```', 'ytvideo', '.mp3', 'https://tikdown.org/getAjax?', 'length', 'fileName', 'packname', 'headers', 'audio', '7PyNOtM', '*Provide Repo Url, Ex:- _.gitclone https://github.com/Sithuawa/SITHUWA-MD_*', '❎ Video file too big!', 'find', '  *Here\'s Your File*\x0a', 'finish', 'result', 'stickers', '*song not Found, Try Differ Name*', 'all', 'yts', 'mimetype', 'audioBitrate', 'mfire', '❎Please provide me a url', 'size', 'log', '518201qffOvi', 'Downloads image from pinterest.', 'audio/mp4', 'downloader', 'mediafire.com', 'img', ' seconds\x0aKeep in mind that there is a chance of a ban if used frequently', 'Suhail-Md', 'random', 'Downloads telegram stickers.', 'ytsearch', 'google-tts-api', '<text|image name>', 'videos', 'itag', '*Give A Number Example: ', 'photo', 'Gives descriptive info of query from youtube..', 'could not found anything', 'sticker', '33215aEaqLO', 'HEAD', 'error', 'mime', 'play', '\x0aAuthor : ', 'https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/', '99WAbsrO', 'video •ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ•', 'ytvid', '\x09  *GOOGLE DRIVE DOWNLOADER SITHUWA-MD*  \x0a', 'tts', 'href', 'floor', 'file_path', 'Animated stickers are not supported', 'reply', '/addstickers/', 'addstickers', '950870mxqtQC', '*._\x0aIf you wants more to download then use Like \x0a\x0a .tgs ', 'thumbnail', 'ytv', '/zipball', 'Example: ', '*Uhh Please, Provide Valid Repositry Url*', '\x0aDescription : ', 'NaN', 'Here it is🧚‍♂️', '4173952CbWaym', '1041800hTaUXQ', './ytsong.mp3', 'data', 'axios', 'is_animated', 'file_id', 'value', '211117duABrL', 'The link you provided is invalid', 'createWriteStream', 'request', 'unlinkSync' ];
+  _0x5540 = function ()
+  {
+    return _0x65ca4d;
+  };
+  return _0x5540();
+}
+cmd(
+{
+  'pattern': _0x1e5a07( 0x1fc ),
+  'alias': [ _0x1e5a07( 0x217 ) ],
+  'desc': _0x1e5a07( 0x27a ),
+  'category': _0x1e5a07( 0x22c ),
+  'react': '🎶',
+  'filename': __filename,
+  'use': _0x1e5a07( 0x297 )
+},
+	async ( _0x2d20cf, _0x533e3e, _0x2a57f1 ) =>
+{
+  const _0x296b19 = _0x1e5a07;
+  if ( !_0x2a57f1 ) return await _0x533e3e[ 'reply' ]( '*_👼 PLease, Give Me Song Name_*' );
+  let _0x159cf6 = require( 'secktor-pack' ),
+    _0x2c58e7 = await _0x159cf6( _0x2a57f1 ),
+    _0x529e8c = _0x2c58e7[ _0x296b19( 0x221 ) ][ 0x1 ],
+    _0x2300eb = _0x296b19( 0x26f ) + _0x529e8c[ _0x296b19( 0x2af ) ] + _0x296b19( 0x1d7 ) + _0x529e8c[ _0x296b19( 0x26b ) ] + _0x296b19( 0x257 ) + _0x529e8c[ _0x296b19( 0x1cd ) ] + _0x296b19( 0x27c ) + _0x529e8c[ 'views' ] + _0x296b19( 0x208 ) + _0x529e8c[ _0x296b19( 0x29c ) ] + _0x296b19( 0x242 ) + _0x529e8c[ 'author' ][ _0x296b19( 0x1f3 ) ] + '\x0a\x0a\x0aReply 1 To Video \x0aReply 2 To Audio';
+  _0x2d20cf[ 'sendMessage' ]( _0x533e3e[ _0x296b19( 0x28f ) ],
+  {
+    'image':
+    {
+      'url': _0x529e8c[ _0x296b19( 0x252 ) ]
+    },
+    'caption': _0x2300eb
+  } );
 
 
 
